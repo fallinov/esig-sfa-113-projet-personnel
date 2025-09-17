2aa27014f62c910bdf5fa04c2b1e1ab9ec93dfcd
Steve Fallet <steve.fallet@divtec.ch>
2025-09-17 11:51:27 +0200

    ajout couer dans icones

--- js/lucide.js
+++ js/lucide.js
@@ -191,6 +191,10 @@
     ['rect', { width: '20', height: '14', x: '2', y: '6', rx: '2' }]
   ];
 
+  const Heart = [
+    ['path', { d: 'M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 1 0-7.78 7.78L12 21.23l8.84-8.84a5.5 5.5 0 0 0 0-7.78z' }]
+  ];
+
   const Code = [
     ['path', { d: 'm16 18 6-6-6-6' }],
     ['path', { d: 'm8 6-6 6 6 6' }]
@@ -241,6 +245,7 @@
     Settings,
     Brain,
     Briefcase,
+    Heart,
     Code,
     Smartphone,
     Layers,
